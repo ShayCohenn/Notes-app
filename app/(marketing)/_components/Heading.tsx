1a76fd444dc65d92bee51d969c24390654e1b308
ShayCohenn <shay91847@gmail.com>
2023-10-22 18:09:23 +0300

    navbar + authentication

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents & Plans. <br />
+        Welcme to <span className="underline">Shay's Notes App</span>
+      </h1>
+      <h3
+        className="text-base sm:text-xl md:text-2xl
+      font-medium">
+        This is a connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          <Link href="/documents">Enter App</Link>
+          <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Started For Free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+    </div>
+  );
+};
+
+export default Heading;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain:"https://funky-badger-76.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
